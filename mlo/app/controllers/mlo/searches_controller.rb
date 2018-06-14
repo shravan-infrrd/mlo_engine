0edbab34f3b977f10fbcd11acfa9f3b941a2ab51
“shravanc” <shravan007.c@gmail.com>
2018-06-14 12:53:14 +0530

    added url to the response

--- mlo/app/controllers/mlo/searches_controller.rb
+++ mlo/app/controllers/mlo/searches_controller.rb
@@ -8,12 +8,13 @@ module Mlo
 		def index
 			subdomain = request.subdomains(0).first 
       subdomain = subdomain.gsub('-tdbank', '').to_s rescue ""
-      user = User.select(mlo_details_params).find_by( :subdomain => subdomain )
+      #user = User.select(mlo_details_params).find_by( :subdomain => subdomain )
+      user = User.find_by( :subdomain => subdomain )
 			if user.nil?
         render json: { message: "user not found"}, status: :unprocessable_entity
         return
 			end
-      mlo_data = user.as_json
+      mlo_data = user.as_json(only: mlo_details_params )
       mlo_data["url"] = user.photo.url
       render json: mlo_data
       return 
