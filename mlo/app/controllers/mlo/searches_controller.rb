7fd6d9ebba2da320bf48381115b3714a2ed7fed6
“shravanc” <shravan007.c@gmail.com>
2018-06-19 16:30:54 +0530

    account_id instance variable

--- mlo/app/controllers/mlo/searches_controller.rb
+++ mlo/app/controllers/mlo/searches_controller.rb
@@ -10,14 +10,13 @@ module Mlo
       subdomain = subdomain.gsub('-tdbank', '').to_s rescue ""
       #user = User.select(mlo_details_params).find_by( :subdomain => subdomain )
       user = User.find_by( :subdomain => subdomain )
-      user = @users = User.last
-
 			if user.nil?
         render json: { message: "user not found"}, status: :unprocessable_entity
         return
 			end
       mlo_data = user.as_json(only: mlo_details_params )
       mlo_data["url"] = user.photo.url
+      @account_id = user.accounts.last.id
       #render json: mlo_data
       #return 
 		end
