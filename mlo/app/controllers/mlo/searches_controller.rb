b618844efcedf257b1df149f18300a5bd5c39ba4
“shravanc” <shravan007.c@gmail.com>
2018-06-19 15:47:14 +0530

    details page changes

--- mlo/app/controllers/mlo/searches_controller.rb
+++ mlo/app/controllers/mlo/searches_controller.rb
@@ -10,14 +10,16 @@ module Mlo
       subdomain = subdomain.gsub('-tdbank', '').to_s rescue ""
       #user = User.select(mlo_details_params).find_by( :subdomain => subdomain )
       user = User.find_by( :subdomain => subdomain )
+      user = @users = User.last
+
 			if user.nil?
         render json: { message: "user not found"}, status: :unprocessable_entity
         return
 			end
       mlo_data = user.as_json(only: mlo_details_params )
       mlo_data["url"] = user.photo.url
-      render json: mlo_data
-      return 
+      #render json: mlo_data
+      #return 
 		end
 
 
--- mlo/lib/tasks/mlo_tasks.rake
+++ mlo/lib/tasks/mlo_tasks.rake
@@ -1,4 +1,9 @@
-# desc "Explaining what the task does"
-# task :mlo do
-#   # Task goes here
-# end
+desc "updating subdomain"
+task :mlo do
+  User.all.each do |user|
+    first_part = user.first_name.split('').first rescue = ""
+    second_part = user.last_name.parameterize rescue ""
+    user.subdomain = first_part + second_part
+    user.save
+  end
+end
