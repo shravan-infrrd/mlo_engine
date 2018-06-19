a9ea74b73cab9dde3217a59ead5a0093427437bb
“shravanc” <shravan007.c@gmail.com>
2018-06-19 12:25:29 +0530

    find function integrated

--- mlo/app/controllers/mlo/users_controller.rb
+++ mlo/app/controllers/mlo/users_controller.rb
@@ -4,41 +4,42 @@ module Mlo
   class UsersController < ApplicationController
 
 
-    def index1
-      @users = User.all
-    end
-
-
     def index
-      @users = User
-  
-      if params.has_key?"term"
-        @users = params[:term].present? ? find_officer_loan(@users) : User.all
-      else
-        @users = User.all
-      end
-
+      @users = find_mlo_results 
     end
- 
-    def find_officer_loan(users)
+
+
+    def find_mlo_results
+      users = User.with_role(:lender)
+      users = users.joined_with_account
       q = "%#{params[:term]}%".upcase
       puts "-----------here q"
       puts q
-      users = users.joins('JOIN mlo_county_users m on m.user_id = users.id')
-                   .joins('JOIN mlo_counties c on c.id = m.county_id')
-                   .joins('JOIN mlo_cities ci on ci.county_id = c.id')
-                   .where('upper(first_name) LIKE ? OR ' \
+      users = users.joins('JOIN mappings m on m.user_id = users.id')
+                  .joins('JOIN counties c on c.id = m.county_id')
+                  .joins('JOIN cities ci on ci.county_id = c.id')
+                  .where('upper(first_name) LIKE ? OR ' \
                           'upper(last_name) LIKE ? OR ' \
                           'ci.zipcode = ? OR ' \
                           'upper(ci.city_name) LIKE ? OR ' \
                           'upper(c.county_name) LIKE ? OR ' \
                           "upper(first_name) || ' ' || upper(last_name) || ' ' || ci.zipcode || ' ' || upper(ci.city_name) || ' ' || upper(c.county_name) LIKE ?",
-                           q, q, q, q, q, q)
+                         q, q, q, q, q, q)
       # users = User.search(params[:assign_search_field], users) if params[:assign_search_field]
-      #users = users.with_role(:lender).paginate(page: params[:page], per_page: 5)
-      users.load
+      users = users.paginate(page: params[:page], per_page: 5)
       users
     end
+
+    def find
+      users = User.all
+      users = find_loan(users)
+      term = params[:term]
+      if(is_number?( term ))
+        render json: users.as_json(:only => [:zipcode, :first_name, :last_name, :city_name, :county_name, :state_name])
+      else
+        render json: users.as_json(:only => [:first_name, :last_name, :city_name, :county_name, :state_name])
+      end
+    end 
 
 
   end
