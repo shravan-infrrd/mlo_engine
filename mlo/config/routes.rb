d7af91827ebf667ee59c400ee09c6bc73a2ee210
“shravanc” <shravan007.c@gmail.com>
2018-06-19 14:50:28 +0530

    added routes

--- mlo/config/routes.rb
+++ mlo/config/routes.rb
@@ -3,6 +3,7 @@ Mlo::Engine.routes.draw do
   root to: "searches#index"
   resources :searches
   get 'users/show_mlo_results' => "users#show_mlo_results"
+  get 'users/find' => "users#find"
   resources :users, only: [:index]
 
 end
